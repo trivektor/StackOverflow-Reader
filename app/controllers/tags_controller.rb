4f50b73a64452f6906b44bf4f3643bde98e64266
trivektor <trivektor@gmail.com>
2014-01-12 23:28:46 -0800

    fetch and display top tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,5 @@
-class TagsController < UIViewController
+class TagsController < BaseController
 
-  include UIViewControllerExtension
-  
   def init
     super
     @tags = []
@@ -14,15 +12,18 @@ class TagsController < UIViewController
     super
     performHousekeepingTasks
     registerEvents
+    Tag.top
   end
 
   def performHousekeepingTasks
+    super
     navigationItem.title = 'Tags'
     @table = createTable(cell: QuestionCell)
     view.addSubview(@table)
   end
 
   def registerEvents
+    'TagsFetched'.add_observer(self, 'displayTags:')
   end
 
   def numberOfSectionsInTableView(tableView)
@@ -31,6 +32,23 @@ class TagsController < UIViewController
 
   def tableView(tableView, numberOfRowsInSection: section)
     @tags.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    cell = @table.dequeueReusableCellWithIdentifier('Cell') || begin
+      QuestionCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: 'Cell')
+    end
+
+    tag = @tags[indexPath.row]
+
+    cell.textLabel.text = "#{tag.name} (#{tag.formatted_count})"
+    cell.textLabel.font = 'HelveticaNeue-Light'.uifont(14)
+    cell
+  end
+
+  def displayTags(notification)
+    @tags = notification.object
+    @table.reloadData
   end
 
 end
---
+++ app/models/tag.rb
@@ -0,0 +1,38 @@
+class Tag
+  include AFNetworkingClient
+
+  attr_accessor :data
+
+  def initialize(data={})
+    @data = data
+    initAFNetworkingClient
+  end
+
+  def count
+    @data[:count].to_i
+  end
+
+  def name
+    @data[:name]
+  end
+
+  def formatted_count
+    count < 1000 ? count : count.to_i.string_with_style
+  end
+
+  def self.top(options={})
+    @@client ||= AFMotion::Client.build(STACK_EXCHANGE_API_HOST) do
+      header 'Accept', 'application/json'
+      response_serializer :json
+    end
+
+    @@client.get('tags', site: STACK_OVERFLOW_SITE_PARAM, filter: 'withbody') do |result|
+      'TagsFetched'.post_notification(result.object[:items].to_a.map { |q| self.new(q) })
+    end
+  end
+
+  def to_json(options={})
+    @data.merge(formatted_count: formatted_count)
+  end
+
+end
